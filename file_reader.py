cad7ca7162c611ff56ec6b0d3a2b5f1b2a41e0fb
Colindy <cnellinger@yahoo.com>
2023-04-24 20:43:13 -0400

    Changes

---
+++ file_reader.py
@@ -0,0 +1,12 @@
+filename = 'pi_digits.txt'
+
+with open(filename) as file_object:
+    contents = file_object.read()
+
+print(contents.rstrip())
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+
+
