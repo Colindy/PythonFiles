1d64ea5a94e471ec338cc713059d22d655905f6e
Colindy <cnellinger@yahoo.com>
2023-05-02 20:09:40 -0400

    Learning about exceptions now.

--- file_reader.py
+++ file_reader.py
@@ -1,12 +1,19 @@
-filename = 'pi_digits.txt'
+filename = 'test.txt'
+filename2 = 'random.txt'
 
-with open(filename) as file_object:
-    contents = file_object.read()
+try:
+    with open(filename, encoding='utf-8') as file_object:
+        contents = file_object.read()
+except FileNotFoundError:
+    print(f"Sorry, {filename} does not appear to exist.")
+else:
+    print(contents.rstrip())
 
-print(contents.rstrip())
-
-with open(filename) as file_object:
+try:
+    with open(filename2, encoding='utf-8') as file_object:
+        contents = file_object.read()
+except FileNotFoundError:
+    print(f"Sorry, {filename2} does not exist.")
+else:
     for line in file_object:
         print(line.rstrip())
-
-
