ae23027df95b0eadc4abeb94871b73ccd551a4cb
cnell <cnellinger@yahoo.com>
2023-05-07 19:19:38 -0400

    Made a couple word counters.  One with a static file and then one that loops input from a user.  Does appear to be limited to the running directory though.

---
+++ word_count.py
@@ -0,0 +1,20 @@
+prompt = "Please list a filename.\nEnter 'q' to quit.\n> "
+
+def count_words(filename):
+    """Here running a simple word counter on a given file"""
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, {filename} does not exist.")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file, {filename} contains {num_words} words in it.")
+
+while True:
+    filename = input(prompt)
+    if filename == 'q':
+        break
+    else:
+        count_words(filename)
