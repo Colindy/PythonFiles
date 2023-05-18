f59af753f59d11ce40bd9a5f936cc009237c1391
Colindy <cnellinger@yahoo.com>
2023-05-18 14:14:19 -0400

    added a reminder to adding.py for funsies

--- adding.py
+++ adding.py
@@ -1,10 +1,13 @@
 prompt1 = "We're going to add some numbers.\nEnter 'q' to quit."
 prompt2 = "Give me the first number: "
 prompt3 = "Give me the second number: "
+counter = 0
 
 print(prompt1)
 
 while True:
+
+    counter += 1
 
     x = input(prompt2)
     if x == 'q':
@@ -19,3 +22,7 @@ while True:
         print(x + y)
     except ValueError:
         print("I'm sorry, we need numbers to do addition.")
+
+    if counter == 3:
+        counter = 0
+        print("Remeber, you can quit by entering 'q'.")
