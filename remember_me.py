3a935ca12f560404c727d6e2b8d9799caac0781f
Colindy <cnellinger@yahoo.com>
2023-07-03 00:13:50 -0400

    refractored remember_me.py

--- remember_me.py
+++ remember_me.py
@@ -1,16 +1,31 @@
 import json
 
-def greet_user():
+def get_stored_username():
+    """Get stored username if available."""
     filename = 'uname.json'
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
-        username = input("What's your name? ")
-        with open(filename, 'w') as f:
-            json.dump(username, f)
-            print(f"We'll remember you, {username}!")
+        return None
     else:
+        return username
+
+def get_new_username():
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    filename = 'uname.json'
+    with open(filename, 'w') as f:
+        json.dump(username, f)
+    return username
+
+def greet_user():
+    """Greet user by name."""
+    username = get_stored_username()
+    if username:
         print(f"Welcome back, {username}!")
+    else:
+        username = get_new_username()
+        print(f"We'll remember you, {username}!")
 
 greet_user()
