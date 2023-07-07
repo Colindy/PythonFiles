0b1b9a53f59d389044c8917e57fb5bdfad19b3e1
cnell <cnellinger@yahoo.com>
2023-07-07 11:22:10 -0400

    Finished DIY 10-12

--- favNumber.py
+++ favNumber.py
@@ -2,12 +2,29 @@ import json
 
 def get_fav_number():
     """Get the favorite number."""
-    filename = favnum.json
+    filename = 'favnum.json'
     try:
         with open(filename) as f:
-            favnum = json.load()
+            favnum = json.load(f)
     except FileNotFoundError:
         return None
     else:
         return favnum
 
+def get_new_favnum():
+    """Gets a new favorite number as input."""
+    favnum = input("What's your favorite number? ")
+    filename = 'favnum.json'
+    with open(filename, 'w') as f:
+        json.dump(favnum, f)
+    return favnum
+
+def print_favnum():
+    """The main function that brings it all together."""
+    favnum = get_fav_number()
+    if favnum:
+        print(f"Your favorite number is {favnum}!")
+    else:
+        get_new_favnum()
+
+print_favnum()
